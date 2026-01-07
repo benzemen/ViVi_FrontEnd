92635e125b797e84d416e1ff86c60a007987bb03
benzeman <benzeman88@gmail.com>
2026-01-07 22:58:53 +0530

    Fix create room request body

--- src/config/AxiosHelper.js
+++ src/config/AxiosHelper.js
@@ -1,5 +1,7 @@
+
 import axios from "axios";
-export const baseURL = "https://vivi-backend-20o3.onrender.com";
+
+export const baseURL = import.meta.env.VITE_API_BASE_URL; // NO quotes!
 export const httpClient = axios.create({
-  baseURL: baseURL,
+    baseURL: baseURL,
 });
--- src/services/RoomService.js
+++ src/services/RoomService.js
@@ -1,8 +1,9 @@
 import { httpClient } from "../config/AxiosHelper";
 
-
 export const createRoomApi = async (roomDetail) => {
-    const response = await httpClient.post(`/api/v1/rooms`, roomDetail);
+    const response = await httpClient.post(`/api/v1/rooms`, {
+        roomId: roomDetail,
+    });
     return response.data;
 };
 
